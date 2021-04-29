5dac321171c778febe731f69291849a6e60befba
Vishnu Suresh <vishnusuresh19012000@gmail.com>
2021-04-29 22:57:16 +0530

    testing the working

--- JAVS_tranScripter/test.py
+++ JAVS_tranScripter/test.py
@@ -1,4 +1,18 @@
-from JAVSutil import JAVSExceptions
+# from JAVSutil import JAVSExceptions
 
-if __name__ == '__main__':
-    raise  JAVSExceptions.JAVSError()
+# if __name__ == '__main__':
+#     raise  JAVSExceptions.JAVSError()
+
+tempString="add $v1, $v2"
+
+def addFun(*args):
+    for identifer in args:
+        print(identifer)
+# tempinput=tempString.strip(" ")
+
+def commaRepalce(stringInput):
+    return stringInput.replace(","," and")
+
+print(commaRepalce(tempString))
+
+# addFun("a","v")
