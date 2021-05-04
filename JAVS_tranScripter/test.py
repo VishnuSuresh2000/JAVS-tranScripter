eeb17e8c6c65d0e7b8ab6a05fb8b59bfed72a61b
Vishnu Suresh <vishnusuresh19012000@gmail.com>
2021-05-04 15:15:55 +0530

    added a demo code of how to work Co-authored-by: Julien Joseph Thomas <Julienjthomas@users.noreply.github.com>

--- JAVS_tranScripter/test.py
+++ JAVS_tranScripter/test.py
@@ -3,12 +3,8 @@
 # if __name__ == '__main__':
 #     raise  JAVSExceptions.JAVSError()
 
-tempString="add $v1, $v2"
+tempString="add $v1, $v2 and $v3"
 
-def addFun(*args):
-    # for identifer in args:
-    #     print(identifer)
-    print(" ".join([f'{x} +' for x in args]))
 
 
 def commaRepalce(stringInput):
@@ -16,15 +12,96 @@ def commaRepalce(stringInput):
 
 # print(commaRepalce(tempString))
 tempinput=commaRepalce(tempString).split(" ")
-grap=[]
+# grap=[]
 print(tempinput)
-for word in tempinput:
-    if word == "add":
-        grap.append(word)
-    elif word =="and" :
-        if "$" in grap[-1]:
-            pass
+# for word in tempinput:
+#     if word == "add":
+#         grap.append(word)
+#     elif word =="and" :
+#         if "$" in grap[-1]:
+#             pass
 
+class CustomTree():
+    def __init__(self,data):
+        self.left=None
+        self.right=None
+        self.data=data
+    
+    def insertNode(self,value):
+        if self.right is None:
+            # print("right is none ",value)
+            self.right=CustomTree(value)
+        else:
+            if "$" in self.right.data:
+                # print("right is not none and $ ",value)
+                temp=self.right
+                self.right=CustomTree(value)
+                self.right.left=temp
+            else:
+                # print("right is not none ",value)
+                self.right.insertNode(value)
+    def PrintTree(self,nodePostion="root",height=0):
+        print(f'{self.data} : {nodePostion}, Height : {height}'),
+        if self.left:
+            self.left.PrintTree(nodePostion="left",height=height+1)
+        if self.right:
+            self.right.PrintTree(nodePostion="right",height=height+1)
+
+def makeTape(node:CustomTree):
+    tape=[]
+    while node.data is not None:
+        tape.append(node.data)
+        if node.left is not None:
+            tape.append(node.left.data)
+        if node.right is not None:
+            node=node.right
+        else:
+            break
+    return tape
+
+            
+itrString=iter(tempinput)
+node=CustomTree(next(itrString))
+try:
+    while True:
+        node.insertNode(next(itrString))
+except StopIteration:
+    pass
+
+print("\nPrint Tree\n")
+
+
+
+node.PrintTree()
+tape =makeTape(node)
+
+
+print('\nPrint Tape\n\n',tape)
+
+variableload=[]
+
+def addFun(*args):
+    # for identifer in args:
+    #     print(identifer)
+    print(" ".join(["result =", *[f'{x} +' for x in args]]))
+
+def andFun(*args):
+    pass
+
+def turingMachine(tape):
+    for cell in reversed(tape):
+        # print(cell)
+        if "$" in cell:
+            # print(f'$ detected {cell}')
+            variableload.append(cell)
+        elif cell == "add":
+            addFun(*variableload)
+        elif cell == "and":
+            andFun(variableload)
+
+turingMachine(tape)
+
+# print("\nLoaded Variables",variableload)
 
 
 
