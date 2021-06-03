f4cdab624d29fe64073b7186cd312810e1bf93f4
Vishnu Suresh <vishnusuresh19012000@gmail.com>
2021-06-03 10:47:21 +0530

    added store in to words in arrithamtic env

--- Jupyter_Files/main.py
+++ Jupyter_Files/main.py
@@ -1,29 +1,31 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
-#Importing Needed Libaries
+# Importing Needed Libaries
 import nltk
 import numpy as np
 
 
 # %%
-#importing data for nltk
+# importing data for nltk
 nltk.download('punkt')
 
 
 # %%
 # Input String
-inputStr='''add 3, 5 .
+inputStr = '''
+store 2 in $v1 .
+add 3, 5 and $v1 .
 print result.
 '''
-print("\nInput String :-\n",inputStr)
+print("\nInput String :-\n", inputStr)
 # inputStr
 
 
 # %%
-#making Tokens of input 
-tokenizeInput=np.array(nltk.word_tokenize(inputStr))
-print("\nTokenzied Input :- \n",tokenizeInput,"\n")
+# making Tokens of input
+tokenizeInput = np.array(nltk.word_tokenize(inputStr))
+print("\nTokenzied Input :- \n", tokenizeInput, "\n")
 
 
 # %%
@@ -31,130 +33,163 @@ print("\nTokenzied Input :- \n",tokenizeInput,"\n")
 
 class CustomTree():
     def __init__(self):
-        self.left=None
-        self.right=None
-        self.data=None
-    
-    def insertNode(self,value):
+        self.left = None
+        self.right = None
+        self.data = None
+
+    def insertNode(self, value):
         if self.data is None:
-            self.data=value
+            self.data = value
         elif self.right is None:
             # print("right is none ",value)
-            self.right=CustomTree()
+            self.right = CustomTree()
             self.right.insertNode(value=value)
         else:
             if "$" in self.right.data:
                 # print("right is not none and $ ",value)
-                temp=self.right
-                self.right=CustomTree()
+                temp = self.right
+                self.right = CustomTree()
                 self.right.insertNode(value=value)
-                self.right.left=temp
+                self.right.left = temp
             else:
                 # print("right is not none ",value)
                 self.right.insertNode(value)
-    def PrintTree(self,nodePostion="root",height=0):
+
+    def PrintTree(self, nodePostion="root", height=0):
         print(f'{self.data} : {nodePostion}, Height : {height}'),
         if self.left:
-            self.left.PrintTree(nodePostion="left",height=height+1)
+            self.left.PrintTree(nodePostion="left", height=height+1)
         if self.right:
-            self.right.PrintTree(nodePostion="right",height=height+1)
+            self.right.PrintTree(nodePostion="right", height=height+1)
 
-def makeTape(node:CustomTree):
-    tape=[]
+
+def makeTape(node: CustomTree):
+    tape = []
     while node.data is not None:
         tape.append(node.data)
         if node.left is not None:
             tape.append(node.left.data)
         if node.right is not None:
-            node=node.right
+            node = node.right
         else:
             break
     return tape
 
 
 # %%
-#Word as function For Arthimetic Env
+# Word as function For Arthimetic Env
 class ArithameticEnv:
     def __init__(self):
         pass
+
     @staticmethod
     def addFun(*args):
-        result =" ".join(["result =", *[f'{x if "$" not in x else x[1:]} +' for x in args]])
-        result=result[:-1]
+        result = " ".join(
+            ["result =", *[f'{x if "$" not in x else x[1:]} +' for x in args]])
+        result = result[:-1]
         return result
+
+    @staticmethod
+    def multiplyFun(*args):
+        result = " ".join(
+            ["result =", *[f'{x if "$" not in x else x[1:]} *' for x in args]])
+        result = result[:-1]
+        return result
+
+    @staticmethod
+    def storeFun(*args):
+        if len(args) == 2:
+            # if str(args[1]).isnumeric():
+            result = f'{args[0] if "$" not in args[0] else args[0][1:]}={args[1] if "$" not in args[1] else args[1][1:]}'
+            # else:
+            # raise Exception("Second argument must be integer")
+            return result
+        else:
+            raise Exception("Only One variable and one Integer is support")
+
+    @staticmethod
+    def toFun(*args):
+        return None
+
+    @staticmethod
+    def inFun(*args):
+        return None
+
     @staticmethod
     def andFun(*args):
         return None
+
     @staticmethod
     def commaFun(*args):
         ArithameticEnv.andFun(*args)
+
     @staticmethod
     def printfun(*args):
-        result =" ".join(["print(", *[f'{x if "$" not in x else x[1:]} ' for x in args],")"])
+        result = " ".join(
+            ["print(", *[f'{x if "$" not in x else x[1:]}' for x in args], ")"])
         return result
-    env_Variables=["result"]
-    env_Words_and_WordAsFunction={"add":addFun.__func__,"and":andFun.__func__,",":commaFun.__func__,"print":printfun.__func__}
+    env_Variables = ["result"]
+    env_Words_and_WordAsFunction = {"add": addFun.__func__, "and": andFun.__func__, ",": commaFun.__func__, "print": printfun.__func__,
+                                    "multiply": multiplyFun.__func__, "store": storeFun.__func__, "to": toFun.__func__, "in": inFun.__func__}
 # ArithameticEnv.addFun("v1","v2")
-#ArithameticEnv.addFun("$v1","v1")
+# ArithameticEnv.addFun("$v1","v1")
 # ArithameticEnv.env_Words_and_WordAsFunction['add']("$v1","v1")
 
 
 # %%
 # Making of Tape from Sentence
-global_Tape=[]
-itreate_each_word=iter(tokenizeInput)
-catch_variable_value=False
-end_Of_a_Sentence=True
-node=None
+global_Tape = []
+itreate_each_word = iter(tokenizeInput)
+catch_variable_value = False
+end_Of_a_Sentence = True
+node = None
 try:
     while True:
-        current_Word=next(itreate_each_word)
-        if "$"== current_Word and not catch_variable_value:
-            catch_variable_value=True
-        elif "."==current_Word and not end_Of_a_Sentence:
-            end_Of_a_Sentence=True
+        current_Word = next(itreate_each_word)
+        if "$" == current_Word and not catch_variable_value:
+            catch_variable_value = True
+        elif "." == current_Word and not end_Of_a_Sentence:
+            end_Of_a_Sentence = True
             print("\nCustom Tree Stracture of the current Sentence :- \n")
             node.PrintTree()
-            tape=makeTape(node=node)
+            tape = makeTape(node=node)
             global_Tape.append(tape)
-            print("\nEnd of a sentence","\nTape :- \n",tape,"\n")
+            print("\nEnd of a sentence", "\nTape :- \n", tape, "\n")
         else:
             if end_Of_a_Sentence:
                 print("\nStarting of a sentence\n")
-                end_Of_a_Sentence=False
-                node=CustomTree()
+                end_Of_a_Sentence = False
+                node = CustomTree()
             elif catch_variable_value:
-                catch_variable_value=False
-                current_Word="$"+current_Word
+                catch_variable_value = False
+                current_Word = "$"+current_Word
             elif str(current_Word).isnumeric():
-                current_Word="$"+current_Word
+                current_Word = "$"+current_Word
             elif current_Word in ArithameticEnv.env_Variables:
-                current_Word="$"+current_Word
+                current_Word = "$"+current_Word
             node.insertNode(current_Word)
             # print(current_Word)
 except StopIteration:
     print("\nEnd of All the lines\n")
-    print("\nGlobal Tape :-\n",global_Tape)
+    print("\nGlobal Tape :-\n", global_Tape)
     pass
 
 
 # %%
-#The machice to Transfer the natural languae to Python code
-pythonCode=[]
-varibleBuffer=[]
+# The machice to Transfer the natural languae to Python code
+pythonCode = []
+varibleBuffer = []
 for sentenceTape in global_Tape:
-    varibleBuffer=[]
+    varibleBuffer = []
     for word in reversed(sentenceTape):
-        if "$" in word :
+        if "$" in word:
             varibleBuffer.append(word)
         else:
-            result=ArithameticEnv.env_Words_and_WordAsFunction[word](*varibleBuffer)
+            result = ArithameticEnv.env_Words_and_WordAsFunction[word](
+                *varibleBuffer)
             if result is not None:
-               pythonCode.append(result)
-# pythonCode
-
-
-
+                pythonCode.append(result)
+pythonCode
 
 
 # %%
@@ -163,6 +198,3 @@ exec("\n".join(pythonCode))
 
 
 # %%
-
-
-
